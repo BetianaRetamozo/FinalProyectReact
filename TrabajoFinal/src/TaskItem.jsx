59d5c18359b9bf5765e32c4f11188756d4ad0ecf
BetianaRetamozo <betianaretamozo@gmail.com>
2023-11-23 00:38:10 -0300

    prueba eliminar item

--- TrabajoFinal/src/TaskItem.jsx
+++ TrabajoFinal/src/TaskItem.jsx
@@ -4,15 +4,18 @@ import './index.css';
 const TaskItem = ({ task }) => {
   const [completed, setCompleted] = useState(false);
 
-  const handleComplete = () => {
-    setCompleted(!completed);
+  const handleComplete = (id) => {
+    const filteredTasks = task.filter (task => task.id !== id)
+    setTasks(filteredTasks)
 
   };
 
   return (
     <div className={`task-item ${completed ? 'completed' : ''}`}>
       {task.name}
-      <button onClick={handleComplete}>Eliminar</button>
+      <button onClick={handleComplete (task.id)}>
+        Eliminar
+      </button>
     </div>
   );
 };
